11449a43757ae9dd850f62265705f325df96fea3
Sven Urbach <urbachsven@gmail.com>
2023-10-12 13:44:59 +0200

    init

---
+++ packages/bht-su-theme/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'bht-su-theme',
+    'description' => 'Sitepackage for TYPO3 v11',
+    'category' => 'templates',
+    'author' => 'Sven Urbach',
+    'author_email' => 'urbachsven@gmail.com',
+    'uploadfolder' => '0',
+    'clearCacheOnLoad' => 1,
+    'createDirs' => '',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.1-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
